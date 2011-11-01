c766f769c1a161cf412f32536204678b3d4b7a33
Peter Peterson <peterfpeterson@gmail.com>
2011-11-01 09:19:29 -0400

    Added real checks for file existence. Refs #3997.

--- StressTestFramework/stresstesting.py
+++ StressTestFramework/stresstesting.py
@@ -91,12 +91,32 @@ class MantidStressTest(object):
         '''
         print self.PREFIX + self.DELIMITER + name + self.DELIMITER + str(value) + '\n',
         
+    def __fileExists(self, filename, path):
+        for direc in path:
+            temp = os.path.join(direc, filename)
+            if os.path.exists(temp):
+                return True
+        return os.path.exists(filename)
+
     def __verifyRequiredFiles(self):
+        # first see if there is anything to do
+        reqFiles = self.requiredFiles()
+        if len(reqFiles) <= 0:
+            return
+
         # by default everything is ok
         foundAll = True
 
-        for filename in self.requiredFiles():
-            if not os.path.isfile(filename): # TODO should check data path
+        # get the data directories to look in
+        from MantidFramework import mtd
+        mtd.initialise()
+        datasearch = mtd.settings['datasearch.directories']
+        if len(datasearch) > 0:
+            datasearch = datasearch.split(';')
+
+        # check that all of the files exist
+        for filename in reqFiles:
+            if not self.__fileExists(filename, datasearch):
                 print "Missing required file: '%s'" % filename
                 foundAll = False
 
