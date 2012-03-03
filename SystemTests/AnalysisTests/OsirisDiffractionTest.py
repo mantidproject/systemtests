5ec0b74e13a8282a98dc42713dd18f6ffcc6bfa0
Peter Parker <peter.parker@stfc.ac.uk>
2012-03-03 11:02:35 +0000

    Refs #4820 - Update system test and test result.

--- SystemTests/AnalysisTests/OsirisDiffractionTest.py
+++ SystemTests/AnalysisTests/OsirisDiffractionTest.py
@@ -1,34 +1,14 @@
 import stresstesting
 from mantidsimple import *
 
-import osiris_diffraction_reducer as odr
-
 class OsirisDiffractionTest(stresstesting.MantidStressTest):
     
   def runTest(self):
-    # View the ASCII file in Test\Data\ReferenceData\Osiris\ to see how this
-    # looks from Arial.
-    reducer = odr.OsirisDiffractionReducer()
-    reducer.append_data_file('OSI89813.raw') # d1
-    reducer.append_data_file('OSI89814.raw') # d2
-    reducer.append_data_file('OSI89815.raw') # d3
-    reducer.append_data_file('OSI89816.raw') # d4
-    reducer.append_data_file('OSI89817.raw') # d5
-
-    # Set cal file
-    reducer.set_cal_file('osiris_041_RES10.cal')
-
-    # Add Vanadium files
-    reducer.append_vanadium_file('OSI89757')
-    reducer.append_vanadium_file('OSI89758')
-    reducer.append_vanadium_file('OSI89759')
-    reducer.append_vanadium_file('OSI89760')
-    reducer.append_vanadium_file('OSI89761')
-    reducer.reduce()
-    
-    ws = reducer.result_workspace()
-    
-    RenameWorkspace(ws, 'OsirisDiffractionTest')
+    OSIRISDiffractionReduction(
+	OutputWorkspace="OsirisDiffractionTest",
+	Sample="OSI89813.raw, OSI89814.raw, OSI89815.raw, OSI89816.raw, OSI89817.raw",
+	CalFile="osiris_041_RES10.cal",
+	Vanadium="OSI89757, OSI89758, OSI89759, OSI89760, OSI89761")
 
   def validate(self):
     self.disableChecking.append('Instrument')
