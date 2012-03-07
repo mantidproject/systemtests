beff2af87e1737ef849354149dbe422551cdb6a1
Mathieu Doucet <doucetm@ornl.gov>
2012-03-07 14:41:13 -0500

    Re #4919 adding system test

---
+++ SystemTests/AnalysisTests/REFMReduction.py
@@ -0,0 +1,37 @@
+import stresstesting
+from MantidFramework import *
+mtd.initialise(False)
+from mantidsimple import *
+
+class REFMReduction(stresstesting.MantidStressTest):
+    def runTest(self):
+          #RefMReduction(RunNumbers=9709,
+          ReflectivityReduction(DataRun="REF_M_9709_event.nxs",
+          PolarizedData=True,
+          Instrument="REF_M",
+          
+          NormalizationRun="REF_M_9684_event.nxs",
+          SignalPeakPixelRange=[216, 224],
+          SubtractSignalBackground=True,
+          SignalBackgroundPixelRange=[172, 197],
+          PerformNormalization=True,
+          NormPeakPixelRange=[226, 238],
+          NormBackgroundPixelRange=[130, 183],
+          SubtractNormBackground=False,
+          CropLowResDataAxis=True,
+          LowResDataAxisPixelRange=[86, 159],
+          CropLowResNormAxis=False,
+          NBins=40,
+          Theta=0.086,
+          OutputWorkspace='reflectivity_Off_Off_9709')
+                
+    def validate(self):
+        # Be more tolerant with the output, mainly because of the errors.
+        # The following tolerance check the errors up to the third digit.   
+        self.tolerance = 0.1
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "reflectivity_Off_Off_9709", 'REFMReduction_off_off.nxs'
+
