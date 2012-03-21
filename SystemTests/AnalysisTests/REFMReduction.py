49b2cbf7ae5f6b9823c19a40971dfb4cd73e043b
Mathieu Doucet <doucetm@ornl.gov>
2012-03-21 14:51:34 -0400

    Re #4919 Change system test for new ref reduction implementation

--- SystemTests/AnalysisTests/REFMReduction.py
+++ SystemTests/AnalysisTests/REFMReduction.py
@@ -5,29 +5,31 @@ from mantidsimple import *
 
 class REFMReduction(stresstesting.MantidStressTest):
     def runTest(self):
-          RefMReduction(RunNumbers=9709,
-          NormalizationRunNumber=9684,
-          SignalPeakPixelRange=[216, 224],
-          SubtractSignalBackground=True,
-          SignalBackgroundPixelRange=[172, 197],
-          PerformNormalization=True,
-          NormPeakPixelRange=[226, 238],
-          NormBackgroundPixelRange=[130, 183],
-          SubtractNormBackground=False,
-          CropLowResDataAxis=True,
-          LowResDataAxisPixelRange=[86, 159],
-          CropLowResNormAxis=False,
-          NBins=40,
-          Theta=0.086,
-          OutputWorkspace='reflectivity_Off_Off_9709')
+          RefReduction(DataRun=9709,
+              NormalizationRun=9684,
+              SignalPeakPixelRange=[216, 224],
+              SubtractSignalBackground=True,
+              SignalBackgroundPixelRange=[172, 197],
+              PerformNormalization=True,
+              NormPeakPixelRange=[226, 238],
+              NormBackgroundPixelRange=[130, 183],
+              SubtractNormBackground=False,
+              CropLowResDataAxis=True,
+              CropLowResNormAxis=False,
+              LowResDataAxisPixelRange = [86, 159],
+              NBins=40,
+              Theta=0.086,
+              PolarizedData=True,
+              Instrument="REF_M",
+              OutputWorkspacePrefix='reflectivity')
                 
     def validate(self):
         # Be more tolerant with the output, mainly because of the errors.
         # The following tolerance check the errors up to the third digit.   
-        self.tolerance = 0.2
+        self.tolerance = 0.25
         self.disableChecking.append('Instrument')
         self.disableChecking.append('Sample')
         self.disableChecking.append('SpectraMap')
         self.disableChecking.append('Axes')
-        return "reflectivity_Off_Off_9709", 'REFMReduction_off_off.nxs'
+        return "reflectivity-Off_Off", 'REFMReduction_off_off.nxs'
 
