b6d06d5c67277746fb97d05f751049cf949514ce
Vickie Lynch <lynchve@ornl.gov>
2012-05-09 17:02:13 -0400

    Refs #5255 try test on windows

--- SystemTests/AnalysisTests/CalibrateRectangularDetector_Test.py
+++ SystemTests/AnalysisTests/CalibrateRectangularDetector_Test.py
@@ -1,19 +1,22 @@
 import stresstesting
 from mantidsimple import *
+import datetime
+from time import localtime, strftime
+
 import sys
 
 class PG3Calibration(stresstesting.MantidStressTest):
 
-    def skipTests(self):
+    #def skipTests(self):
         # We skip this test if the system is not Linux for the moment
-        if sys.platform.startswith('win') or sys.platform == 'darwin':
-          return True
-        else:
-          return False
+        #if sys.platform.startswith('win') or sys.platform = = 'darwin':
+          #return True
+        #else:
+          #return False
 
 
     def requiredFiles(self):
-        files = ["PG3_2538_event.nxs","PG3_golden.cal"] 
+        files = ["PG3_2538_event.nxs"] 
         return files
 
     def runTest(self):
@@ -22,24 +25,25 @@ class PG3Calibration(stresstesting.MantidStressTest):
         savedir = os.path.abspath(os.path.curdir)
 
         # run the actual code
-        CalibrateRectangularDetectors(OutputDirectory = savedir, SaveAs='dspacemap and calibration', FilterBadPulses=True,
-                          GroupDetectorsBy='All', DiffractionFocusWorkspace=False, Binning='0.5, -0.0015, 2.5', 
+        CalibrateRectangularDetectors(OutputDirectory = savedir, SaveAs = 'dspacemap and calibration', FilterBadPulses = True,
+                          GroupDetectorsBy = 'All', DiffractionFocusWorkspace = False, Binning = '0.5, -0.0004, 2.5', 
                           PeakPositions = '2.0592,1.2610,1.0754,0.8916,0.8182,0.7280,0.6864,0.6305,0.6029', 
-                         CrossCorrelation =False, Instrument='PG3', RunNumber='2538', Extension='_event.nxs')
-        Rebin(InputWorkspace='PG3_2538_calibrated', OutputWorkspace='PG3_2538_calibrated', Params='0.5, -0.0015, 2.5', PreserveEvents=False)
+                          CrossCorrelation = False, Instrument = 'PG3', RunNumber = '2538', Extension = '_event.nxs')
+        self.saved_cal_file = savedir+"/PG3_calibrate_d2538"+strftime("_%Y_%m_%d.cal")
+        # delete first line with date and time
+        f = open( self.saved_cal_file, 'r' )
+        lines = f.readlines()
+        f.close()
 
-        # load the golden one
-        LoadEventNexus(Filename='PG3_2538_event.nxs',OutputWorkspace='PG3_2538',Precount=True)
-        LoadCalFile(InputWorkspace='PG3_2538', CalFileName='PG3_golden.cal', WorkspaceName='PG3_2538')
-        FilterBadPulses(InputWorkspace='PG3_2538', OutputWorkspace='PG3_2538')
-        CompressEvents(InputWorkspace='PG3_2538', OutputWorkspace='PG3_2538', Tolerance=0.01)
-        MaskDetectors(Workspace='PG3_2538', MaskedWorkspace='PG3_2538_mask')
-        AlignDetectors(InputWorkspace='PG3_2538', OutputWorkspace='PG3_2538', OffsetsWorkspace='PG3_2538_offsets')
-        Rebin(InputWorkspace='PG3_2538', OutputWorkspace='PG3_2538', Params='0.5, -0.0015, 2.5', PreserveEvents=False)
+        f = open( self.saved_cal_file, 'w' )
+        f.write( ''.join( lines[1:] ) )
+        f.close()
 
 
     def validateMethod(self):
-        return "ValidateWorkspaceToWorkspace"
+        return "ValidateASCII"
 
     def validate(self):
-        return ('PG3_2538_calibrated','PG3_2538')
+        return self.saved_cal_file, \
+            os.path.join(os.path.dirname(__file__), 'ReferenceResults','PG3_golden.cal')
+
