6338609ba896afc895a7aeaaed99665b45b753c9
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-05-11 17:37:37 +0100

    Add system test for offspec scripts. Refs #5156

---
+++ SystemTests/AnalysisTests/OffspecSESANS.py
@@ -0,0 +1,18 @@
+from stresstesting import MantidStressTest
+from mantidsimple import mtd
+import offspec
+
+class OffspecSESANS(MantidStressTest):
+    
+    def requiredFiles(self):
+        return ["OFFSPEC00010791.raw","OFFSPEC00010792.raw","OFFSPEC00010793.raw"]
+    
+    def runTest(self):
+        binning=["2.0","0.2","12.0","2"]
+        mtd.settings["default.instrument"] = "OFFSPEC"
+        offspec.nrSESANSP0Fn("10792","P055","109","119","2","1",binning)
+        offspec.nrSESANSFn("10791+10793","dPMMA","","P055pol",
+                           "100","130","2","1","2","3009.9",binning,"2","0")
+        
+    def validate(self):
+        return "dPMMASESANS","OffspecSESANS.nxs"
---
+++ SystemTests/AnalysisTests/OffspecSESANSP0.py
@@ -0,0 +1,19 @@
+from stresstesting import MantidStressTest
+from mantidsimple import mtd
+import offspec
+
+class OffspecSESANSP0(MantidStressTest):
+    
+    def requiredFiles(self):
+        return ["OFFSPEC00010792.raw"]
+    
+    def runTest(self):
+        binning=["2.0","0.2","12.0","2"]
+        mtd.settings["default.instrument"] = "OFFSPEC"
+        offspec.nrSESANSP0Fn("10792","P055","109","119","2","1",binning)
+        
+    def cleanup(self):
+        pass
+        
+    def validate(self):
+        return "P055pol","OffspecSESANSP0.nxs"
