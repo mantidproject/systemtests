b05c41df1bbb9e61ab5aa7800a3dde1c5c5f1049
Mathieu Doucet <doucetm@ornl.gov>
2012-05-16 11:08:27 -0400

    Re #5347 cleanup

--- SystemTests/AnalysisTests/HFIRReduction.py
+++ SystemTests/AnalysisTests/HFIRReduction.py
@@ -3,9 +3,6 @@ from MantidFramework import *
 mtd.initialise(False)
 from mantidsimple import *
 from reduction.instruments.sans.hfir_command_interface import *
-import math
-
-TEST_DIR = "../Data/SANS2D/"
 
 class HFIRReduction(stresstesting.MantidStressTest):
     """
@@ -16,7 +13,6 @@ class HFIRReduction(stresstesting.MantidStressTest):
 
         mtd.settings['default.facility'] = 'HFIR'
         HFIRSANS()
-        #DataPath(TEST_DIR)
         DirectBeamCenter("BioSANS_empty_cell.xml")
         AppendDataFile("BioSANS_test_data.xml")
         SetTransmission(0.51944, 0.011078)
@@ -24,10 +20,6 @@ class HFIRReduction(stresstesting.MantidStressTest):
         AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
         Reduce1D()
         
-        #SaveAscii(Filename="tmp.txt", InputWorkspace="BioSANS_test_data_Iq")
-        #LoadAscii('tmp.txt', "output")
-        #LoadAscii('AnalysisTests/ReferenceResults/BioSANS_test_data_Iq.txt', "ref")
-
     def validate(self):
         self.disableChecking.append('Instrument')
         self.disableChecking.append('Sample')
