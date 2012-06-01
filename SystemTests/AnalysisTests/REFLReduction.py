d40cbc9a48642c2b9fa3b97eb30ed245bd8af1bf
Jean Bilheux <bilheuxjm@ornl.gov>
2012-06-01 09:45:19 -0400

    Added REFL system test. This refs #4303

---
+++ SystemTests/AnalysisTests/REFLReduction.py
@@ -0,0 +1,35 @@
+import stresstesting
+from MantidFramework import *
+mtd.initialise(False)
+from mantidsimple import *
+
+class REFLReduction(stresstesting.MantidStressTest):
+    def runTest(self):
+        RefLReduction(RunNumbers=[int(70977)],
+                      NormalizationRunNumber=70964,
+                      SignalPeakPixelRange=[125, 135],
+                      SubtractSignalBackground=True,
+                      SignalBackgroundPixelRange=[122, 138],
+                      NormFlag=True,
+                      NormPeakPixelRange=[125, 135],
+                      NormBackgroundPixelRange=[122, 138],
+                      SubtractNormBackground=True,
+                      LowResDataAxisPixelRangeFlag=True,
+                      LowResDataAxisPixelRange=[115, 210],
+                      LowResNormAxisPixelRangeFlag=True,
+                      LowResNormAxisPixelRange=[115, 210],
+                      TOFRange=[50343.0, 62140.0],
+                      IncidentMediumSelected='H2O',
+                      QMin=0.001,
+                      QStep=-0.001,
+                      AngleOffset=0.009,
+                      AngleOffsetError=0.001,
+                      OutputWorkspace='reflectivity_70977')
+                
+    def validate(self):
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "reflectivity-70977", 'REFLReduction.nxs'
+
