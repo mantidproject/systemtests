d8e46c3e541c5dafa9e781ede18b6f5cd6b46663
Vickie Lynch <lynchve@ornl.gov>
2012-06-28 10:44:00 -0400

    Refs #5460 try larger bins

--- SystemTests/AnalysisTests/CalibrateRectangularDetector_Test.py
+++ SystemTests/AnalysisTests/CalibrateRectangularDetector_Test.py
@@ -4,7 +4,22 @@ import datetime
 from time import localtime, strftime
 import os
 
+def _skip_test():
+    """Helper function to determine if we run the test"""
+    import platform
+    # Only runs on RHEL6 at the moment
+    if "Linux" not in platform.platform():
+        return True
+    flavour = platform.linux_distribution()[2]
+    if flavour == 'Santiago': # Codename for RHEL6
+        return False # Do not skip
+    else:
+        return True
+
 class PG3Calibration(stresstesting.MantidStressTest):
+
+    def skipTests(self):
+        return _skip_test()
 
     def requiredFiles(self):
         files = ["PG3_2538_event.nxs"] 
@@ -17,7 +32,7 @@ class PG3Calibration(stresstesting.MantidStressTest):
 
         # run the actual code
         CalibrateRectangularDetectors(OutputDirectory = savedir, SaveAs = 'calibration', FilterBadPulses = True,
-                          GroupDetectorsBy = 'All', DiffractionFocusWorkspace = False, Binning = '0.5, -0.0004, 2.5', 
+                          GroupDetectorsBy = 'All', DiffractionFocusWorkspace = False, Binning = '0.5, -0.0008, 2.5', 
                           PeakPositions = '2.0592,1.2610,1.0754,0.8916,0.8182,0.7280,0.6864,0.6305,0.6029', 
                           CrossCorrelation = False, Instrument = 'PG3', RunNumber = '2538', Extension = '_event.nxs')
 
@@ -38,6 +53,9 @@ class PG3Calibration(stresstesting.MantidStressTest):
 
 class PG3CCCalibration(stresstesting.MantidStressTest):
 
+    def skipTests(self):
+        return _skip_test()
+
     def requiredFiles(self):
         files = ["PG3_2538_event.nxs"] 
         return files
@@ -50,7 +68,7 @@ class PG3CCCalibration(stresstesting.MantidStressTest):
         # run the actual code
 
         CalibrateRectangularDetectors(OutputDirectory = savedir, SaveAs = 'calibration', FilterBadPulses = True,
-                          GroupDetectorsBy = 'All', DiffractionFocusWorkspace = False, Binning = '0.5, -0.0004, 2.5',
+                          GroupDetectorsBy = 'All', DiffractionFocusWorkspace = False, Binning = '0.5, -0.0008, 2.5',
                           PeakPositions = '0.7282933,1.261441',DetectorsPeaks = '17,6',
                           CrossCorrelation = True, Instrument = 'PG3', RunNumber = '2538', Extension = '_event.nxs')
 
