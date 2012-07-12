2989efb272c5835897473dd8992cf73bf3800139
Owen Arnold <owen.arnold@stfc.ac.uk>
2012-07-12 08:56:03 +0100

    refs #5515. Disable instrument checking

--- SystemTests/AnalysisTests/CRISPLoadingTest.py
+++ SystemTests/AnalysisTests/CRISPLoadingTest.py
@@ -4,6 +4,11 @@ from LoadAndCheckBase import *
 Test File loading and basic data integrity checks of CRISP data in Mantid.
 '''
 class CRISPLoadingTest(LoadAndCheckBase):
+
+    def __init__(self):
+        super(self.__class__,self).__init__()
+        self.disableChecking.append("Instrument")
+
     def get_raw_workspace_filename(self):
         return "CSP85423.raw"
         
