813db843fc88508820c52a907d887fd411c64414
Robert Whitley <robert.whitley@stfc.ac.uk>
2012-07-13 15:50:50 +0100

    Refs #5499, #5500, #5501, and #5502. Add grouping tests.

--- SystemTests/AnalysisTests/ISISMuonAnalysisGrouping.py
+++ SystemTests/AnalysisTests/ISISMuonAnalysisGrouping.py
@@ -5,7 +5,7 @@ from mantidsimple import *
 from abc import ABCMeta, abstractmethod
 
 #----------------------------------------------------------------------
-class ISISMuonAnalysis(stresstesting.MantidStressTest):
+class ISISMuonAnalysisGrouping(stresstesting.MantidStressTest):
     """A base class for the ISIS Muon Analysis tests
     
     The workflow is defined in the runTest() method, simply
@@ -17,11 +17,9 @@ class ISISMuonAnalysis(stresstesting.MantidStressTest):
         - sample_run: An integer run number of the sample
         - period_data: A boolean denoting whether the file has period data.
         - asym: A boolean to tell whether the plot type is assymetry or not.
+        - logs: A boolean to tell whether the plot type is logorithmic or not.
         - x_min: Float value of the minimum x.
         - x_max: Float value of the maximum x.
-        - rebin: Boolean to tell whether rebinning is to be done.
-        - rebin_fixed: Optional boolean to tell if the rebinning is in fixed steps.
-        - rebin_params: A string containing the rebin parameters. See wiki rebin for more info.
     """
     __metaclass__ = ABCMeta # Mark as an abstract class
 
@@ -52,33 +50,14 @@ class ISISMuonAnalysis(stresstesting.MantidStressTest):
         GroupDetectors(InputWorkspace='MuonAnalysis', OutputWorkspace=outputWS, MapFile=self.map_name)
       CropWorkspace(InputWorkspace=outputWS, OutputWorkspace=outputWS, XMin=self.x_min, XMax=self.x_max)
       CloneWorkspace(InputWorkspace=outputWS, OutputWorkspace=(outputWS + '_Raw') )
-      
-      # Rebin then...
-      if (self.rebin ):
-      
-        ws = mtd[outputWS]
-        binSize = ws.dataX(0)[1]-ws.dataX(0)[0]
-        firstX = ws.dataX(0)[0]
-        lastX = ws.dataX(0)[ws.blocksize()]
-        
-        if (self.rebin_fixed):          
-          Rebin(InputWorkspace=outputWS, OutputWorkspace=outputWS, Params=str(binSize*float(self.rebin_params) ) )
-        else:
-          Rebin(InputWorkspace=outputWS, OutputWorkspace=outputWS, Params=self.rebin_params)      
-        
-        numberOfFullBunchedBins = math.floor((lastX - firstX) / binSize )
-        
-        # ...Crop
-        if(numberOfFullBunchedBins > 0):
-          lastX = firstX + numberOfFullBunchedBins*binSize
-          lastX_str = '%.15f' % lastX
-          CropWorkspace(InputWorkspace=outputWS, OutputWorkspace=outputWS, XMax=lastX_str )
-      
       GroupWorkspaces(InputWorkspaces=outputWS + ',' + outputWS + '_Raw', OutputWorkspace='MuonGroup')
-      
-      if(self.asym):
-        AsymmetryCalc(InputWorkspace=outputWS, OutputWorkspace=outputWS, ForwardSpectra='0', BackwardSpectra='1')
 
+      if(self.logs):
+        Logarithm(InputWorkspace=outputWS, OutputWorkspace=outputWS)
+      if(self.asym):
+        RemoveExpDecay(InputWorkspace=outputWS, OutputWorkspace=outputWS)
+      
+      
     def validate(self):
       """Returns the name of the workspace & file to compare"""
       self.tolerance = 1e-7
@@ -91,99 +70,101 @@ class ISISMuonAnalysis(stresstesting.MantidStressTest):
     def _validate_properties(self):
       """Check the object properties are
       in an expected state to continue
-      """
-      if type(self.instr_name) != str:
-        raise RuntimeError("instr_name property should be a string")
+      """      
       if type(self.file_name) != str:
         raise RuntimeError("file_name property should be a string")
+      if type(self.map_name) != str:
+        raise RuntimeError("map_name property should be a string")
+      if type(self.instr_name) != str:
+        raise RuntimeError("instr_name property should be a string")
       if type(self.period_data) != bool:
         raise RuntimeError("period_data property should be a bool")
+      if type(self.asym) != bool:
+        raise RuntimeError("asym property should be a bool")
+      if type(self.logs) != bool:
+        raise RuntimeError("log property should be a bool")
 
 
 
-#------------------------- ARGUS tests -------------------------------------------------
+#------------------------- ARGUS group fwd test -------------------------------------------------
 
-class ARGUSAnalysisFromFile(ISISMuonAnalysis):
+class ARGUSAnalysisFromFile(ISISMuonAnalysisGrouping):
 
   def __init__(self):
-    ISISMuonAnalysis.__init__(self)
+    ISISMuonAnalysisGrouping.__init__(self)
     self.file_name = 'argus0044309.nxs'
-    self.map_name = 'ARGUSGrouping.xml'
+    self.map_name = 'ARGUSFwdGrouping.xml'
     self.instr_name = 'ARGUS'
     self.sample_run = 44309
-    self.asym = True
     self.period_data = False
-    self.x_min = 2
-    self.x_max = 12
-    self.rebin = True
-    self.rebin_fixed = True
-    self.rebin_params = '1'
+    self.asym = False
+    self.logs = True
+    self.x_min = 3
+    self.x_max = 10
     
   def get_reference_file(self):
-    return "ARGUSAnalysis.nxs"
+    return "ARGUSAnalysisLogFwd.nxs"
 
 
-#------------------------- EMU tests -------------------------------------------------
+#------------------------- EMU group fwd test -------------------------------------------------
 
-class EMUAnalysisFromFile(ISISMuonAnalysis):
+class EMUAnalysisFromFile(ISISMuonAnalysisGrouping):
 
   def __init__(self):
-    ISISMuonAnalysis.__init__(self)
+    ISISMuonAnalysisGrouping.__init__(self)
     self.file_name = 'emu00031895.nxs'
-    self.map_name = 'EMUGrouping.xml'
+    self.map_name = 'EMUFwdGrouping.xml'
     self.instr_name = 'EMU'
     self.sample_run = 31895
-    self.asym = True
     self.period_data = True
+    self.asym = True
+    self.logs = False
     self.x_min = 0.11
     self.x_max = 10
-    self.rebin = False
+
  
   def get_reference_file(self):
-    return "EMUAnalysis.nxs"
+    return "EMUAnalysisAsymFwd.nxs"
 
 
-#------------------------- HiFi tests -------------------------------------------------
+#------------------------- HiFi group 0 test -------------------------------------------------
 
-class HiFiAnalysisFromFile(ISISMuonAnalysis):
+class HiFiAnalysisFromFile(ISISMuonAnalysisGrouping):
 
   def __init__(self):
-    ISISMuonAnalysis.__init__(self)
+    ISISMuonAnalysisGrouping.__init__(self)
     self.file_name = 'hifi00038401.nxs'
-    self.map_name = 'HiFiGrouping.xml'
+    self.map_name = 'HiFi0Grouping.xml'
     self.instr_name = 'Hifi'
     self.sample_run = 38401
-    self.asym = True
     self.period_data = False
-    self.x_min = 1
-    self.x_max = 5
-    self.rebin = True
-    self.rebin_fixed = True
-    self.rebin_params = '1'
+    self.asym = True
+    self.logs = False
+    self.x_min = 0.1199
+    self.x_max = 7.4999
  
   def get_reference_file(self):
-    return "HiFiAnalysis.nxs"
+    return "HiFiAnalysisAsym0.nxs"
     
 
-#------------------------- MuSR tests -------------------------------------------------
+#------------------------- MuSR Group 1 test -------------------------------------------------
 
-class MuSRAnalysisFromFile(ISISMuonAnalysis):
+class MuSRAnalysisFromFile(ISISMuonAnalysisGrouping):
 
   def __init__(self):
-    ISISMuonAnalysis.__init__(self)
+    ISISMuonAnalysisGrouping.__init__(self)
     self.file_name = 'MUSR00015192.nxs'
-    self.map_name = 'MuSRGrouping.xml'
+    self.map_name = 'MuSR1Grouping.xml'
     self.instr_name = 'MuSR'
     self.sample_run = 15192
-    self.asym = True
     self.period_data = True
-    self.x_min = 0.11
-    self.x_max = 10
-    self.rebin = True
-    self.rebin_fixed = False
-    self.rebin_params = '0.11,0.0159999,10'
+    self.asym = False
+    self.logs = True
+    self.x_min = 1.4
+    self.x_max = 3.9
+
  
   def get_reference_file(self):
-    return "MuSRAnalysis.nxs"
+    return "MuSRAnalysisLog1.nxs"
  
  
