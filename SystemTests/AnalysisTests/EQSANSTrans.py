135584af52fed5a2b80fc2a90043b173c4d6500d
Mathieu Doucet <doucetm@ornl.gov>
2012-09-17 17:53:38 -0400

    Re #5436 add test for recent bug

--- SystemTests/AnalysisTests/EQSANSTrans.py
+++ SystemTests/AnalysisTests/EQSANSTrans.py
@@ -69,3 +69,40 @@ class EQSANSTransmissionEvent(EQSANSTransmission):
         Scale(InputWorkspace="EQSANS_1466_event_Iq", Factor=2777.81, 
               Operation='Multiply', OutputWorkspace="EQSANS_1466_event_Iq")              
 
+class EQSANSTransmissionCompatibility(EQSANSTransmission):
+    """
+        Analysis Tests for EQSANS
+        Check that the transmission correction can be applied if the 
+        sample run and transmission runs don't have the same binning
+    """
+    
+    def runTest(self):
+        """
+            Check that transmission correction returns the correct workspace
+        """
+        # Note that the EQSANS Reducer does the transmission correction by default,
+        # so we are also testing the EQSANSTransmission algorithm
+        mtd.settings['default.facility'] = 'SNS'
+        EQSANS(True)
+        AppendDataFile("EQSANS_1466_event.nxs")
+        SolidAngle()
+        UseConfig(False)
+        UseConfigTOFTailsCutoff(False)
+        UseConfigMask(False)
+        TotalChargeNormalization(normalize_to_beam=False)
+        DirectBeamTransmission("EQSANS_4061_event.nxs", "EQSANS_4061_event.nxs", beam_radius=3)
+        ThetaDependentTransmission(True)
+        Reduce1D()
+        # Scale up to match correct scaling.
+        Scale(InputWorkspace="EQSANS_1466_event_Iq", Factor=2777.81, 
+              Operation='Multiply', OutputWorkspace="EQSANS_1466_event_Iq")              
+
+    def validate(self):
+        # Be more tolerant with the output, mainly because of the errors.
+        # The following tolerance check the errors up to the third digit.   
+        self.tolerance = 0.1
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "EQSANS_1466_event_Iq", 'EQSANSTransmissionCompatibility.nxs'
