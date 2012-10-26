8cace33bacb1152597e0c7eb3f7853d9a6751fce
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2012-10-26 11:12:33 +0100

    Re-enable TobyFitResolutionSimulationTest .Refs #5813

--- SystemTests/AnalysisTests/TobyFitResolutionSimulationTest.py
+++ SystemTests/AnalysisTests/TobyFitResolutionSimulationTest.py
@@ -20,26 +20,24 @@ class TobyFitResolutionSimulationTest(MantidStressTest):
     _success = False
 
     def skipTests(self):
-        return True
+        return False
 
     def requiredMemoryMB(self):
-        return 6000
+        return 3000
 
     def runTest(self):
         ei = 300.
         bins = [-30,3,279]
         temperature = 6. 
         chopper_speed = 600.
+          
+        # Oriented lattice & goniometer.
+        alatt = 5.57
+        blatt = 5.51
+        clatt = 12.298
+        uvec = [9.700000e-03,9.800000e-03,9.996000e-01]
+        vvec = [9.992000e-01,-3.460000e-02,-4.580000e-02]
 
-        # Oriented lattice & goniometer. Mantid axes are Beam=Z,Perp=X,Y=Up
-        # The tobyfit lattice parameters have been permuted accordingly
-
-        alatt = 5.51 
-        blatt = 12.298
-        clatt = 5.57
-        uvec = [9.800000e-03,9.996000e-01,9.700000e-03]
-        vvec = [-3.460000e-02,-4.580000e-02,9.992000e-01]
-        
         omega = 0.0
         alpha = 0.0
         beta = 0.0
@@ -53,46 +51,60 @@ class TobyFitResolutionSimulationTest(MantidStressTest):
         # Crystal mosaic
         eta_sig = 4.0
 
-        fake_data = CreateSimulationWorkspace(Instrument='MERLIN',BinParams=bins,UnitX='DeltaE',
-                                              DetectorTableFilename='MER06399.raw')
+        data_dir = '/home/dmn58364/Scripts/inelastic/tobyfit/'
+        fake_data = CreateSimulationWorkspace(Instrument='MERLIN',
+                                              BinParams=bins,UnitX='DeltaE',
+                                              DetectorTableFilename=data_dir+'MER04466.raw')
 
-        # Required log entries, can be taken from real ones by placing an instrument parameter of the same
-        # name pointing to the log name
+        ##
+        ## Required log entries, can be taken from real ones by placing an instrument parameter of the same
+        ## name pointing to the log name
+        ##
         AddSampleLog(Workspace=fake_data, LogName='Ei',LogText=str(ei), LogType="Number")
         AddSampleLog(Workspace=fake_data, LogName='temperature_log',LogText=str(temperature), LogType="Number")
         AddSampleLog(Workspace=fake_data, LogName='chopper_speed_log',LogText=str(chopper_speed), LogType="Number")
         AddSampleLog(Workspace=fake_data, LogName='eta_sigma',LogText=str(eta_sig), LogType="Number")
-
-        # Sample shape
+        
+        ##
+        ## Sample shape
+        ##
         CreateSampleShape(InputWorkspace=fake_data, ShapeXML=create_cuboid_xml(sx,sy,sz))
-
-        # Chopper & Moderator models. 
+        
+        ##
+        ## Chopper & Moderator models. 
+        ##
         CreateModeratorModel(Workspace=fake_data,ModelType='IkedaCarpenterModerator',
                              Parameters="TiltAngle=32,TauF=2.7,TauS=0,R=0")
         CreateChopperModel(Workspace=fake_data,ModelType='FermiChopperModel',
                            Parameters="AngularVelocity=chopper_speed_log,ChopperRadius=0.049,SlitThickness=0.0023,SlitRadius=1.3,Ei=Ei,JitterSigma=0.0")
-
-        # UB matrix
-        SetUB(Workspace=fake_data,a=alatt,b=blatt,c=clatt,u=uvec,v=vvec)
-
         
-        # Sample rotation. Simulate 1 run at zero degrees psi
+        ##
+        ## UB matrix
+        ##
+        SetUB(Workspace=fake_data,a=alatt,b=blatt,c=clatt,u=uvec,v=vvec)
+        
+        ##
+        ## Sample rotation. Simulate 1 run at zero degrees psi
+        ##
+        
         psi = 0.0
         AddSampleLog(Workspace=fake_data,LogName='psi',LogText=str(psi),LogType='Number')
         SetGoniometer(Workspace=fake_data,Axis0="psi,0,1,0,1")
+        
         # Create the MD workspace
-        fake_md = ConvertToMD(InputWorkspace=fake_data, QDimensions="Q3D", SplitInto=[3], SplitThreshold=100,
-                              MinValues="-15,-15,-15,-30", MaxValues="25,25,25,279")
-
+        qscale = 'Q in A^-1'
+        fake_md = ConvertToMD(InputWorkspace=fake_data, QDimensions="Q3D", QConversionScales=qscale,
+                              SplitInto=[3], SplitThreshold=100,
+        		      MinValues="-15,-15,-15,-30", MaxValues="25,25,25,279",OverwriteExisting=True)
+        
         # Run the simulation.
         resol_model = "TobyFitResolutionModel"
         xsec_model = "Strontium122"
-        parameters = "Seff=0.7,J1a=38.7,J1b=-5.0,J2=27.3,SJc=10.0,GammaSlope=0.08,MultEps=0,TwinType=0,MCLoopMax=10"
+        parameters = "Seff=0.7,J1a=38.7,J1b=-5.0,J2=27.3,SJc=10.0,GammaSlope=0.08,MultEps=0,TwinType=0,MCLoopMin=10,MCLoopMax=10,MCType=1" # Use sobol & restart each pixel to ensure reproducible result
         simulated = SimulateResolutionConvolvedModel(InputWorkspace=fake_md,
                                                      ResolutionFunction=resol_model,
                                                      ForegroundModel=xsec_model,
                                                      Parameters=parameters)
-
         # Take a slice
         slice_ws = BinMD(InputWorkspace=simulated,
                       AlignedDim0='[H,0,0], -12.000000, 9.000000, 100',
