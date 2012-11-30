d54d3368ae7aa0f818f6115f278f23cc6b95539c
Mathieu Doucet <doucetm@ornl.gov>
2012-11-30 11:58:19 -0500

    Re #6151 Add system tests for HFIR API2

--- SystemTests/AnalysisTests/HFIREffAPIv2.py
+++ SystemTests/AnalysisTests/HFIREffAPIv2.py
@@ -3,26 +3,26 @@ import mantid
 from mantid.simpleapi import *
 from reduction_workflow.instruments.sans.hfir_command_interface import *
 
-class HFIRReductionAPIv2(stresstesting.MantidStressTest):
-    """
-        Simple reduction example
-    """
-    
+class HFIREffAPIv2(stresstesting.MantidStressTest):
     def runTest(self):
-
+        """
+            System test for sensitivity correction
+        """
         config = ConfigService.Instance()
         config["facilityName"]='HFIR'
         GPSANS()
         DirectBeamCenter("BioSANS_empty_cell.xml")
         AppendDataFile("BioSANS_test_data.xml")
         SetTransmission(0.51944, 0.011078)
-        SensitivityCorrection("BioSANS_flood_data.xml")
+        SensitivityCorrection("BioSANS_flood_data.xml", dark_current="BioSANS_dark_current.xml")
         AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
-        Reduce()
-        
+        Reduce1D()
+                
     def validate(self):
+        self.tolerance = 0.00001
         self.disableChecking.append('Instrument')
         self.disableChecking.append('Sample')
         self.disableChecking.append('SpectraMap')
         self.disableChecking.append('Axes')
-        return "BioSANS_test_data_Iq", "HFIRReduction.nxs"
+        return "BioSANS_test_data_Iq", 'HFIREff.nxs'
+
