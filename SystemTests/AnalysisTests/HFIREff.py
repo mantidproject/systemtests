f2a7ec67f4524a8a71441e423fcc441bb2c43318
Mathieu Doucet <doucetm@ornl.gov>
2012-12-06 09:42:53 -0500

    Re #6151 update system tests

--- SystemTests/AnalysisTests/HFIREff.py
+++ SystemTests/AnalysisTests/HFIREff.py
@@ -13,7 +13,8 @@ class HFIREff(stresstesting.MantidStressTest):
         DirectBeamCenter("BioSANS_empty_cell.xml")
         AppendDataFile("BioSANS_test_data.xml")
         SetTransmission(0.51944, 0.011078)
-        SensitivityCorrection("BioSANS_flood_data.xml", dark_current="BioSANS_dark_current.xml")
+        SensitivityCorrection("BioSANS_flood_data.xml",
+                              dark_current="BioSANS_dark_current.xml")
         AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
         Reduce1D()
                 
@@ -24,4 +25,52 @@ class HFIREff(stresstesting.MantidStressTest):
         self.disableChecking.append('SpectraMap')
         self.disableChecking.append('Axes')
         return "BioSANS_test_data_Iq", 'HFIREff.nxs'
+
+class HFIRSensitivityDirectBeamCenter(stresstesting.MantidStressTest):
+    def runTest(self):
+        """
+            System test for sensitivity correction
+        """
+        mtd.settings['default.facility'] = 'HFIR'
+        HFIRSANS()
+        DirectBeamCenter("BioSANS_empty_cell.xml")
+        AppendDataFile("BioSANS_test_data.xml")
+        SetTransmission(0.51944, 0.011078)
+        SensitivityCorrection("BioSANS_flood_data.xml", 
+                              dark_current="BioSANS_dark_current.xml")
+        SensitivityDirectBeamCenter("BioSANS_empty_trans.xml")
+        AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
+        Reduce1D()
+                
+    def validate(self):
+        self.tolerance = 0.00001
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "BioSANS_test_data_Iq", 'HFIRSensitivityDirectBeamCenter.nxs'
+
+class HFIRSensitivityScatteringBeamCenter(stresstesting.MantidStressTest):
+    def runTest(self):
+        """
+            System test for sensitivity correction
+        """
+        mtd.settings['default.facility'] = 'HFIR'
+        HFIRSANS()
+        DirectBeamCenter("BioSANS_empty_cell.xml")
+        AppendDataFile("BioSANS_test_data.xml")
+        SetTransmission(0.51944, 0.011078)
+        SensitivityCorrection("BioSANS_flood_data.xml", 
+                              dark_current="BioSANS_dark_current.xml")
+        SensitivityScatteringBeamCenter("BioSANS_test_data.xml")
+        AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
+        Reduce1D()
+                
+    def validate(self):
+        self.tolerance = 0.00001
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "BioSANS_test_data_Iq", 'HFIRSensitivityScatteringBeamCenter.nxs'
 
--- SystemTests/AnalysisTests/HFIREffAPIv2.py
+++ SystemTests/AnalysisTests/HFIREffAPIv2.py
@@ -26,3 +26,55 @@ class HFIREffAPIv2(stresstesting.MantidStressTest):
         self.disableChecking.append('Axes')
         return "BioSANS_test_data_Iq", 'HFIREff.nxs'
 
+class HFIRSensitivityDirectBeamCenter(stresstesting.MantidStressTest):
+    def runTest(self):
+        """
+            System test for sensitivity correction
+        """
+        config = ConfigService.Instance()
+        config["facilityName"]='HFIR'
+        GPSANS()
+        DirectBeamCenter("BioSANS_empty_cell.xml")
+        AppendDataFile("BioSANS_test_data.xml")
+        SetTransmission(0.51944, 0.011078)
+        SensitivityCorrection("BioSANS_flood_data.xml", 
+                              dark_current="BioSANS_dark_current.xml")
+        SensitivityDirectBeamCenter("BioSANS_empty_trans.xml")
+        AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
+        Reduce1D()
+                
+    def validate(self):
+        self.tolerance = 0.00001
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "BioSANS_test_data_Iq", 'HFIRSensitivityDirectBeamCenter.nxs'
+    
+class HFIRSensitivityScatteringBeamCenter(stresstesting.MantidStressTest):
+    def runTest(self):
+        """
+            System test for sensitivity correction
+        """
+        config = ConfigService.Instance()
+        config["facilityName"]='HFIR'
+        GPSANS()
+        DirectBeamCenter("BioSANS_empty_cell.xml")
+        AppendDataFile("BioSANS_test_data.xml")
+        SetTransmission(0.51944, 0.011078)
+        SensitivityCorrection("BioSANS_flood_data.xml", 
+                              dark_current="BioSANS_dark_current.xml")
+        SensitivityScatteringBeamCenter("BioSANS_test_data.xml")
+        AzimuthalAverage(binning="0.01,0.001,0.11", error_weighting=True)
+        Reduce1D()
+                
+    def validate(self):
+        self.tolerance = 0.00001
+        self.disableChecking.append('Instrument')
+        self.disableChecking.append('Sample')
+        self.disableChecking.append('SpectraMap')
+        self.disableChecking.append('Axes')
+        return "BioSANS_test_data_Iq", 'HFIRSensitivityScatteringBeamCenter.nxs'
+
+
+
