b7b97125472083e83ca9b7f253bfae4788a17287
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-01-24 09:54:50 +0000

    Add PG3 dat file to LoadLotsOfFiles exclusion filter. Refs #6083

--- SystemTests/AnalysisTests/LoadLotsOfFiles.py
+++ SystemTests/AnalysisTests/LoadLotsOfFiles.py
@@ -26,6 +26,7 @@ BANNED_FILES = ['992 Descriptions.txt',
                 'MAP17269.raw', # Don't need to check multiple MAPS files 
                 'MAP17589.raw',
                 'MER06399.raw', # Don't need to check multiple MERLIN files
+                'PG3_11485-1.dat', # Generic load doesn't do very well with ASCII files
                 'PG3_2538_event.nxs', # Don't need to check all of the PG3 files
                 'PG3_9829_event.nxs',
                 'REF_M_9684_event.nxs',
