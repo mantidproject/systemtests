e7c7ccd60e789e31973dd5bfc5297260f34e04e7
Andrei Savici <saviciat@ornl.gov>
2013-02-25 14:13:26 -0500

    More debugging. Refs #5525

--- SystemTests/AnalysisTests/HYSPECReductionTest.py
+++ SystemTests/AnalysisTests/HYSPECReductionTest.py
@@ -30,6 +30,8 @@ class HYSPECReductionTest(stresstesting.MantidStressTest):
 		GenerateGroupingSNSInelastic(AlongTubes="4",AcrossTubes="2",Instrument="HYSPEC",Filename=self.groupingFile)
 		config['default.facility']="SNS"
 		DgsReduction(SampleInputWorkspace='splitc',IncidentBeamNormalisation='ByCurrent',OutputWorkspace='reduced',GroupingFile=self.groupingFile,TimeIndepBackgroundSub ='1',TibTofRangeStart =10400,TibTofRangeEnd =12400,IncidentEnergyGuess=50)
+		SaveNexus('splitc','HYS_split.nxs')
+		SaveNexus('reduced','HYS_reduced.nxs')
 		DeleteWorkspace('splitc')
 		SetGoniometer('reduced',Axis0="s1,0,1,0,1")
 		SetUB('reduced',5.823,6.475,3.186,90,90,90,'0,1,0','0,0,1')
@@ -38,7 +40,6 @@ class HYSPECReductionTest(stresstesting.MantidStressTest):
 		MergeMD(InputWorkspaces='md',OutputWorkspace='merged')
 		DeleteWorkspace("md")
 		BinMD(InputWorkspace='merged',AxisAligned='0',BasisVector0='[H,0,0],in 1.079 A^-1,1,0,0,0',BasisVector1='[0,K,0],in 0.97 A^-1,0,1,0,0',BasisVector2='[0,0,L],in 1.972 A^-1,0,0,1,0',BasisVector3='DeltaE,DeltaE,0,0,0,1',OutputExtents='-3,3,-2,6,-4,-1.5,-3,3',OutputBins='1,100,100,1',Parallel='1',OutputWorkspace='slice')
-		SaveMD('slice','HYSPEC_MD.nxs')
 
 	def validate(self):
 		self.tolerance = 1e-5
