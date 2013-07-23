af01e4557cb6c9d315a1159d69357dac388e04bb
Owen Arnold <owen.arnold@stfc.ac.uk>
2013-07-23 16:15:22 +0100

    refs #7317. Create system test for quick script.

---
+++ SystemTests/AnalysisTests/ReflectometryQuick.py
@@ -0,0 +1,30 @@
+import stresstesting
+from mantid.simpleapi import *
+from isis_reflgui import quick
+
+class ReflectometryQuick(stresstesting.MantidStressTest):
+    """
+    This is a system test for the top-level quick routines. Quick is the name given to the 
+    ISIS reflectometry reduction scripts.
+    
+    """
+    
+    def runTest(self):
+        defaultInstKey = 'default.instrument'
+        defaultInstrument = config[defaultInstKey]
+        try:
+            config[defaultInstKey] = 'INTER'
+            LoadISISNexus(Filename='13463', OutputWorkspace='13463')
+            LoadISISNexus(Filename='13464', OutputWorkspace='13464')
+            LoadISISNexus(Filename='13460', OutputWorkspace='13460')
+    
+            transmissionRuns = '13463,13464'
+            runNo = '13460'
+            incidentAngle = 0.7
+            quick.quick(runNo, trans=transmissionRuns, theta=incidentAngle) 
+        finally:
+            config[defaultInstKey] = defaultInstrument
+            
+    def validate(self):
+        self.disableChecking.append('Instrument')
+        return '13460_IvsQ','QuickReferenceResult.nxs'
