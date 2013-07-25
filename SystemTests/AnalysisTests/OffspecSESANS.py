92525edc425a21a8460c5da54274c59981ca1632
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2013-07-25 18:06:37 +0100

    Only do the Offspec tests if the offspec scripts are available.
    Refs #7522

--- SystemTests/AnalysisTests/OffspecSESANS.py
+++ SystemTests/AnalysisTests/OffspecSESANS.py
@@ -1,13 +1,21 @@
 from stresstesting import MantidStressTest
 from mantid.simpleapi import mtd, config
-import offspec
 
 class OffspecSESANS(MantidStressTest):
     
+    def skipTests(self):
+        skip = False
+        try:
+            import offspec
+        except ImportError:
+            skip = True
+        return skip
+
     def requiredFiles(self):
         return ["OFFSPEC00010791.raw","OFFSPEC00010792.raw","OFFSPEC00010793.raw"]
     
     def runTest(self):
+        import offspec
         binning=["2.0","0.2","12.0","2"]
         config["default.instrument"] = "OFFSPEC"
         offspec.nrSESANSP0Fn("10792","P055","109","119","2","1",binning)
