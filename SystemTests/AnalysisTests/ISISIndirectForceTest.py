ee6647a6697bec44d21573b00e1128e14e41b453
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2013-09-30 16:23:05 +0100

    Refs #5422 remove parameter map checking in system test.

--- SystemTests/AnalysisTests/ISISIndirectForceTest.py
+++ SystemTests/AnalysisTests/ISISIndirectForceTest.py
@@ -6,11 +6,11 @@ from IndirectImport import is_supported_f2py_platform
 #====================================================================================================
 class IN10SiliconTest(stresstesting.MantidStressTest):
 
-    def skipTests(self):
-        if is_supported_f2py_platform():
-            return False
-        else:
-            return True
+    # def skipTests(self):
+    #     if is_supported_f2py_platform():
+    #         return False
+    #     else:
+    #         return True
 
     def runTest(self):
         import IndirectForce as Main 
@@ -25,21 +25,22 @@ class IN10SiliconTest(stresstesting.MantidStressTest):
         verbOp = True
         saveOp = False
         plotOp = False
-        Main.InxStart(instr,run,ana,refl,rejectZ,useM, '',verbOp,plotOp,saveOp)
+        Main.InxStart(instr,run,ana,refl,rejectZ,useM,'',verbOp,plotOp,saveOp)
         
     def validate(self):
         self.tolerance = 1e-2
+        self.disableChecking.append("Instrument")
         return 'IN10_P3OT_350K_silicon111_red', 'ISISIndirectForce_IN10SiliconTest.nxs'
 
 
 #====================================================================================================
 class IN16SiliconTest(stresstesting.MantidStressTest):
 
-    def skipTests(self):
-        if is_supported_f2py_platform():
-            return False
-        else:
-            return True
+    # def skipTests(self):
+    #     if is_supported_f2py_platform():
+    #         return False
+    #     else:
+    #         return True
     
     def runTest(self):
         import IndirectForce as Main
@@ -54,11 +55,12 @@ class IN16SiliconTest(stresstesting.MantidStressTest):
         verbOp = True
         saveOp = False
         plotOp = False
-        Main.IbackStart(instr,run,ana,refl,rejectZ,useM, '',verbOp,plotOp,saveOp)
+        Main.IbackStart(instr,run,ana,refl,rejectZ,useM,'',verbOp,plotOp,saveOp)
 
     def validate(self):
         self.tolerance = 1e-2
         self.disableChecking.append("SpectraMap")
+        self.disableChecking.append("Instrument")
         return 'IN16_65722_silicon111_red', 'ISISIndirectForce_IN16SiliconTest.nxs'
 
 
