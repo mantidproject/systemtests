cf98b57eccf935e8cb300b9dc485855abd4539e3
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2013-10-02 16:05:38 +0100

    Refs #5422 Added system test for MolDyn.

--- SystemTests/AnalysisTests/ISISIndirectForceTest.py
+++ SystemTests/AnalysisTests/ISISIndirectForceTest.py
@@ -55,6 +55,8 @@ class IN13CaFTest(stresstesting.MantidStressTest):
 
         return ( ws1Match and ws2Match )
 
+    # function to check two workspaces match
+    # Used when the result of a test produces more than a single workspace
     def checkWorkspacesMatch(self, ws1, ws2):
         from mantid.simpleapi import SaveNexus, FrameworkManager
         checker = FrameworkManager.createAlgorithm("CheckWorkspacesMatch")
@@ -99,3 +101,69 @@ class IN16SiliconTest(stresstesting.MantidStressTest):
 
 
 #====================================================================================================
+class MolDynCdlTest(stresstesting.MantidStressTest):
+
+    def runTest(self):
+        from MolDynTransfer import MolDynImport
+
+        filename = 'DISF_NaF.cdl'
+        funcNames = 'Fqt-total,Sqw-total'
+        verbOp = True
+        saveOp = False
+        plotOp = False
+
+        MolDynImport(filename,funcNames,verbOp,plotOp,saveOp)
+        
+    def validate(self):
+        self.tolerance = 1e-2
+        self.disableChecking.append("Instrument")
+
+        from mantid.simpleapi import Load
+
+        Load(Filename='ISISIndirectForce_MolDynCDL.nxs',OutputWorkspace='ISISIndirectForce_MolDynCDL')
+        Load(Filename='ISISIndirectForce_MolDynCDL_SQW.nxs',OutputWorkspace='ISISIndirectForce_MolDynCDL_SQW')
+
+        # check each of the resulting workspaces match
+        ws1Match = self.checkWorkspacesMatch('DISF_NaF_Fqt-total', 'ISISIndirectForce_MolDynCDL')
+        ws2Match = self.checkWorkspacesMatch('DISF_NaF_Sqw-total', 'ISISIndirectForce_MolDynCDL_SQW')
+
+        return ( ws1Match and ws2Match )
+
+    # function to check two workspaces match
+    # Used when the result of a test produces more than a single workspace
+    def checkWorkspacesMatch(self, ws1, ws2):
+        from mantid.simpleapi import SaveNexus, FrameworkManager
+        checker = FrameworkManager.createAlgorithm("CheckWorkspacesMatch")
+        checker.setLogging(True)
+        checker.setPropertyValue("Workspace1", ws1)
+        checker.setPropertyValue("Workspace2", ws2)
+        checker.setPropertyValue("Tolerance", str(self.tolerance))
+        checker.setPropertyValue("CheckInstrument","0")
+        
+        checker.execute()
+
+        if checker.getPropertyValue("Result") != 'Success!':
+            print self.__class__.__name__
+            SaveNexus(InputWorkspace=ws2,Filename=self.__class__.__name__+'-mismatch.nxs')
+            return False
+
+        return True
+
+#====================================================================================================
+class MolDynDatTest(stresstesting.MantidStressTest):
+
+    def runTest(self):
+        from MolDynTransfer import MolDynText
+
+        filename = 'WSH_test.dat'
+        verbOp = True
+        saveOp = False
+        plotOp = False
+
+        MolDynText(filename,verbOp,plotOp,saveOp)
+        
+    def validate(self):
+        self.tolerance = 1e-2
+        self.disableChecking.append("Instrument")
+        
+        return 'WSH_test_iqt', 'ISISIndirectForce_MolDynDAT.nxs'
