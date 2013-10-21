863cf45ba119de0a63b37eec1d81f54e83779006
Gesner Passos <gesner.passos@stfc.ac.uk>
2013-10-21 12:12:14 +0100

    LOQSANSUtilityTest: make path system independent
    Windows uses backslashs for path, change it to forward slashes before
    comparing paths.
    
    re #8156

--- SystemTests/AnalysisTests/LOQSANSUtilityTest.py
+++ SystemTests/AnalysisTests/LOQSANSUtilityTest.py
@@ -6,19 +6,21 @@ import SANSadd2 as add
 
 import os
 
+def unixLikePathFromWorkspace(ws):
+    return su.getFilePathFromWorkspace(ws).replace('\\','/')
+
+
 class SANSUtilityTest(stresstesting.MantidStressTest):
 
     def runTest(self):
         # created after issue reported in #8156
         ws = Load('LOQ54432')
-        print su.getFilePathFromWorkspace(ws)
-        self.assertTrue('Data/LOQ/LOQ54432.raw' in su.getFilePathFromWorkspace(ws))
+        self.assertTrue('Data/LOQ/LOQ54432.raw' in unixLikePathFromWorkspace(ws))
         ws = Load('LOQ99618.RAW')
-        print su.getFilePathFromWorkspace(ws)
-        self.assertTrue('Data/LOQ/LOQ99618.RAW' in su.getFilePathFromWorkspace(ws))
+        self.assertTrue('Data/LOQ/LOQ99618.RAW' in unixLikePathFromWorkspace(ws))
         add.add_runs(('LOQ54432','LOQ54432'),'LOQ','.raw')
         ws = Load('LOQ54432-add')
-        file_path =  su.getFilePathFromWorkspace(ws)
+        file_path =  unixLikePathFromWorkspace(ws)
         self.assertTrue('logs/LOQ54432-add' in file_path)
         os.remove(file_path)
         
