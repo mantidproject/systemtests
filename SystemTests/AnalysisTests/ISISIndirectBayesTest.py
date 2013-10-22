1460419daa1db412a8e8f2d37fab5511d375262d
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2013-10-22 14:04:36 +0100

    Refs #7965 Update tolerance for XP

--- SystemTests/AnalysisTests/ISISIndirectBayesTest.py
+++ SystemTests/AnalysisTests/ISISIndirectBayesTest.py
@@ -278,7 +278,7 @@ class QLResNormTest(stresstesting.MantidStressTest):
         Main.QLRun('QL',sname,rname,rsname,erange,nbins,fitOp,wfile,loopOp,verbOp,plotOp,saveOp)
 
     def validate(self):
-        self.tolerance = 1e-4 
+        self.tolerance = 1e-3 
         return 'irs26176_graphite002_QLr_Result','ISISIndirectBayes_QLr_ResNorm_Test.nxs'
 
     def cleanup(self):
@@ -318,7 +318,7 @@ class QLWidthTest(stresstesting.MantidStressTest):
         Main.QLRun('QL',sname,rname,rsname,erange,nbins,fitOp,wfile,loopOp,verbOp,plotOp,saveOp)
 
     def validate(self):
-        self.tolerance = 1e-4 
+        self.tolerance = 1e-3 
         return 'irs26176_graphite002_QLr_Result','ISISIndirectBayes_QLr_width_Test.nxs'
 
     def cleanup(self):
