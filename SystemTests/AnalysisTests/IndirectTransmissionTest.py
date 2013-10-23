504464da84c94dedda083c715923d907a87e26d7
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2013-10-23 12:39:30 +0100

    Refs #8176 Write system test for IndirectTransmission

--- SystemTests/AnalysisTests/IndirectTransmissionTest.py
+++ SystemTests/AnalysisTests/IndirectTransmissionTest.py
@@ -2,10 +2,19 @@ import stresstesting
 from mantid.simpleapi import *
 
 class IndirectTransmissionTest(stresstesting.MantidStressTest):
-    
-    def runTest(self):
-    	pass
 
-    def validate(self):
-        return 'IRIS_graphite_002_Transmission','IndirectTransmissionTest.nxs'
-        
+	def runTest(self):
+		instrument = "IRIS"
+		analyser = "graphite"
+		reflection = "002"
+
+		#using water
+		formula = "H2-O"
+		density = 0.1
+		thickness = 0.1
+
+		IndirectTransmission(Instrument=instrument, Analyser=analyser, Reflection=reflection,
+										ChemicalFormula=formula, NumberDensity=density, Thickness=thickness)
+
+	def validate(self):
+		return 'IRIS_graphite_002_Transmission','IndirectTransmissionTest.nxs'
