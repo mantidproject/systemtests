ba03b012ba7911ff26e1f745d0d86f0151ae0383
Keith Brown <keith.brown@stfc.ac.uk>
2013-11-28 15:36:48 +0000

    Refs #8514 Added SURF Test
    Added the SURF Reflectonomy test and the ReferenceResult asscociated with it

---
+++ SystemTests/AnalysisTests/ReflectometryQuickPointDetectorSURF.py
@@ -0,0 +1,30 @@
+import stresstesting
+from mantid.simpleapi import *
+from isis_reflectometry import quick
+reload(quick)
+
+#class ReflectometryQuick(object):
+class ReflectometryQuick(stresstesting.MantidStressTest):
+    """
+    This is a system test for the top-level quick routines. Quick is the name given to the 
+    ISIS reflectometry reduction scripts.
+    
+    """
+    
+    def runTest(self):
+        defaultInstKey = 'default.instrument'
+        defaultInstrument = config[defaultInstKey]
+        try
+            config[defaultInstKey] = 'SURF'
+            LoadISISNexus(Filename='102951', OutputWorkspace='102951')
+            LoadISISNexus(Filename='102935', OutputWorkspace='102935')
+            transmissionRuns = '102935'
+            runNo = '102951'
+            incidentAngle = 0.25
+            quick.quick(runNo, trans=transmissionRuns, theta=incidentAngle)
+        finally:
+            config[defaultInstKey] = defaultInstrument
+        
+    def validate(self):
+        self.disableChecking.append('Instrument')
+        return '102951_IvsQ','ReflectometryQuickPointDetectorSURFReferenceResult.nxs'
