7e1567340e1774729addad027a999d938fe0efac
Owen Arnold <owen.arnold@stfc.ac.uk>
2014-01-10 10:00:35 +0000

    refs #8689. Fixes L2QTest.

--- SystemTests/AnalysisTests/L2QScriptTest.py
+++ SystemTests/AnalysisTests/L2QScriptTest.py
@@ -12,9 +12,10 @@ class L2QScriptTest(stresstesting.MantidStressTest):
         Io=CropWorkspace(InputWorkspace=ws,XMin=0.8,XMax=14.5,StartWorkspaceIndex=2,EndWorkspaceIndex=2)
         D=CropWorkspace(InputWorkspace=ws,XMin=0.8,XMax=14.5,StartWorkspaceIndex=3)
         I= Divide(LHSWorkspace=D,RHSWorkspace=Io,AllowDifferentNumberSpectra=True)
-        detectorName =  'linear-detector'
+        detector_component_name =  'linear-detector'
+        sample_component_name = 'some-surface-holder'
         theta = 0.7
-        l2q(ws, detectorName, theta) # This generates an output workspace called IvsQ
+        l2q(ws, detector_component_name, theta, sample_component_name) # This generates an output workspace called IvsQ
         
         
     def validate(self):
