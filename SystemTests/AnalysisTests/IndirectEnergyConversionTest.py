d251ccba4fe727a0edd8909c70d71b0ab12b30ad
Dan Nixon <dan@dan-nixon.com>
2014-09-23 15:30:55 +0100

    Changed system test for changes to algorithm parameters
    Refs #10083

--- SystemTests/AnalysisTests/IndirectEnergyConversionTest.py
+++ SystemTests/AnalysisTests/IndirectEnergyConversionTest.py
@@ -10,10 +10,8 @@ class IndirectEnergyConversionTest(stresstesting.MantidStressTest):
         detector_range = [3, 53]
         files = 'irs21360.raw'
         rebin_string = '-0.5,0.005,0.5'
-        output_workspace = 'IndirectEnergyConversionTest'
 
-        InelasticIndirectReduction(OutputWorkspace=output_workspace,
-                                   InputFiles=files,
+        InelasticIndirectReduction(InputFiles=files,
                                    RebiNString=rebin_string,
                                    DetectorRange=detector_range,
                                    Instrument=instrument,
@@ -24,4 +22,4 @@ class IndirectEnergyConversionTest(stresstesting.MantidStressTest):
     def validate(self):
         self.disableChecking.append('Instrument')
         self.disableChecking.append('SpectraMap')
-        return 'IndirectEnergyConversionTest', 'IndirectEnergyConversionTest.nxs'
+        return 'irs21360_graphite002_red', 'IndirectEnergyConversionTest.nxs'
