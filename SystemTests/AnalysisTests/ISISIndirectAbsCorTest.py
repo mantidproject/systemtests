d697aac2ece028c86bef0607934a464acb1f0dd3
Dan Nixon <dan@dan-nixon.com>
2014-11-05 13:53:37 +0000

    Added systemtest for diffraction corrections
    Refs #10094

--- SystemTests/AnalysisTests/ISISIndirectAbsCorTest.py
+++ SystemTests/AnalysisTests/ISISIndirectAbsCorTest.py
@@ -202,3 +202,39 @@ class AbsRunFeederDefaultBeamWidthTest(stresstesting.MantidStressTest):
     def validate(self):
         self.tolerance = 1e-3
         return 'irs26176_graphite002_cyl_Abs', 'ISISIndirectAbsCor_DefaultBeamWidthTest.nxs'
+
+#====================================================================================================
+
+
+class AbsRunFeederDiffractionTest(stresstesting.MantidStressTest):
+    """
+    Test AbsRunFeeder with sample and can material formulas for a diffraction run.
+    """
+
+    def skipTests(self):
+        return not is_supported_f2py_platform()
+
+    def runTest(self):
+        from IndirectAbsCor import AbsRunFeeder
+
+        # H20 sample
+        inputWS = 'irs26176_diffspec_red'
+        # cylindrical Vanadium can
+        canWS = 'irs26173_diffspec_red'
+
+        Load(inputWS + '.nxs', OutputWorkspace=inputWS)
+        Load(canWS + '.nxs', OutputWorkspace=canWS)
+
+        geom = 'cyl'
+        ncan = 2
+        size = [0.2, 0.25, 0.26, 0.0]
+        avar = 0.002
+        density = [0.1, 0.1, 0.1]
+        beam_width = 4.0
+        sampleFormula = 'H2-O'
+        canFormula = 'V'
+        AbsRunFeeder(inputWS, canWS, geom, ncan, size, avar, density, beam_width=beam_width, sample_formula=sampleFormula, can_formula=canFormula,  sigs=[0,0,0], siga=[0,0,0])
+
+    def validate(self):
+        self.tolerance = 1e-3
+        return 'irs26176_diffspec_cyl_Abs', 'ISISIndirectAbsCor_AbsRunFeederDiffractionTest.nxs'
