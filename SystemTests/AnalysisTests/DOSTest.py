4a667fc34776307b72762320dac1ec90a6c53955
Dan Nixon <dan@dan-nixon.com>
2014-12-03 09:15:47 +0000

    Added new DOS system tests
    Refs #9340

--- SystemTests/AnalysisTests/DOSTest.py
+++ SystemTests/AnalysisTests/DOSTest.py
@@ -4,13 +4,27 @@ from mantid.api import *
 from mantid.simpleapi import *
 
 class DOSPhononTest(stresstesting.MantidStressTest):
-    
+
     def runTest(self):
       file_name = 'squaricn.phonon'
       self.ouput_ws_name = 'squaricn'
       self.ref_result = 'II.DOSTest.nxs'
 
-      DensityOfStates(File=file_name,OutputWorkspace=self.ouput_ws_name)
+      DensityOfStates(File=file_name, OutputWorkspace=self.ouput_ws_name)
+
+    def validate(self):
+      return self.ouput_ws_name, self.ref_result
+
+#------------------------------------------------------------------------------------
+
+class DOSPhononCrossSectionScaleTest(stresstesting.MantidStressTest):
+
+    def runTest(self):
+      file_name = 'squaricn.phonon'
+      self.ouput_ws_name = 'squaricn'
+      self.ref_result = 'II.DOSCrossSectionScaleTest.nxs'
+
+      DensityOfStates(File=file_name, ScaleByCrossSection=True, OutputWorkspace=self.ouput_ws_name)
 
     def validate(self):
       return self.ouput_ws_name, self.ref_result
@@ -18,21 +32,21 @@ class DOSPhononTest(stresstesting.MantidStressTest):
 #------------------------------------------------------------------------------------
 
 class DOSCastepTest(stresstesting.MantidStressTest):
-    
+
     def runTest(self):
       file_name = 'squaricn.castep'
       self.ouput_ws_name = 'squaricn'
       self.ref_result = 'II.DOSTest.nxs'
 
       DensityOfStates(File=file_name,OutputWorkspace=self.ouput_ws_name)
-    
+
     def validate(self):
       return self.ouput_ws_name, self.ref_result
 
 #------------------------------------------------------------------------------------
 
 class DOSRamanActiveTest(stresstesting.MantidStressTest):
-    
+
     def runTest(self):
       file_name = 'squaricn.phonon'
       spec_type = 'Raman_Active'
@@ -40,7 +54,7 @@ class DOSRamanActiveTest(stresstesting.MantidStressTest):
       self.ref_result = 'II.DOSRamanTest.nxs'
 
       DensityOfStates(File=file_name, SpectrumType=spec_type, OutputWorkspace=self.ouput_ws_name)
-    
+
     def validate(self):
       self.tolerance = 1e-3
       return self.ouput_ws_name, self.ref_result
@@ -48,7 +62,7 @@ class DOSRamanActiveTest(stresstesting.MantidStressTest):
 #------------------------------------------------------------------------------------
 
 class DOSIRActiveTest(stresstesting.MantidStressTest):
-    
+
     def runTest(self):
       file_name = 'squaricn.phonon'
       spec_type = 'IR_Active'
@@ -97,4 +111,41 @@ class DOSPartialSummedContributionsTest(stresstesting.MantidStressTest):
     def validate(self):
       return self.ouput_ws_name, self.ref_result
 
+#------------------------------------------------------------------------------------
 
+class DOSPartialCrossSectionScaleTest(stresstesting.MantidStressTest):
+
+    def runTest(self):
+      file_name = 'squaricn.phonon'
+      spec_type = 'DOS'
+      self.ouput_ws_name = 'squaricn'
+      self.ref_result = 'II.DOSPartialCrossSectionScaleTest.nxs'
+
+      DensityOfStates(File=file_name, SpectrumType=spec_type, Ions="H,C,O", ScaleByCrossSection=True,
+                      OutputWorkspace=self.ouput_ws_name)
+
+    def validate(self):
+      return self.ouput_ws_name, self.ref_result
+
+#------------------------------------------------------------------------------------
+
+class DOSPartialSummedContributionsCrossSectionScaleTest(stresstesting.MantidStressTest):
+    """
+      This test checks the reference result of the total DOS against
+      the summed partial contributions of all elements. The two should be roughly
+      equal to within a small degree of error.
+    """
+
+    def runTest(self):
+
+      file_name = 'squaricn.phonon'
+      spec_type = 'DOS'
+      self.ouput_ws_name = 'squaricn'
+      self.ref_result = 'II.DOSCrossSectionScaleTest.nxs'
+      self.tolerance = 1e-10
+
+      DensityOfStates(File=file_name, SpectrumType=spec_type, Ions="H,C,O", SumContributions=True,
+                      ScaleByCrossSection=True, OutputWorkspace=self.ouput_ws_name)
+
+    def validate(self):
+      return self.ouput_ws_name, self.ref_result
